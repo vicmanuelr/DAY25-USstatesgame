e9924047c8793c0f5b4369345b37cca7db1c0a88
Victor Ponce <vicmanuelr@gmail.com>
2022-09-14 21:08:06 -0600

    Game main brain and check answers finished
    - Convert the guess to title case
    - Check if the guess is among the 50 states
    - Write correct guesses onto the map
    - Use a loop to allow the user to keep guessing
    - Record the correct guesses in a list
    - Keep track of the score

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 # Setup of a U.S. map image on the turtle screen
 screen = turtle.Screen()
@@ -17,3 +18,37 @@ turtle.shape(image)
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
 # # with this code made 50_states.csv coordinates with this method
+
+
+write_turtle = turtle.Turtle()  # Turtle that will write answers on map
+write_turtle.hideturtle()
+list_of_answers = []  # List to check if answer is already given
+data = pd.read_csv("50_states.csv")
+
+
+def valid_answer(user_answer, previous_ans, game_data):
+    if user_answer in previous_ans:
+        return False
+    check_list = list(game_data.state == user_answer)
+    if True in check_list:
+        x_cor = int(game_data[game_data.state == user_answer]["x"])
+        y_cor = int(game_data[game_data.state == user_answer]["y"])
+        write_turtle.penup()
+        write_turtle.goto(x_cor, y_cor)
+        write_turtle.write(user_answer, align="left")
+        previous_ans.append(user_answer)
+        return True
+
+
+# Getting user input for game
+game_is_on = True
+while game_is_on:
+    user_input = screen.textinput(title="Guess the State", prompt="What's another state name:").title()
+    if valid_answer(user_input, list_of_answers, data):
+        screen.title(f"You got {len(list_of_answers)}/50")
+        print(f"You got {len(list_of_answers)}/50")
+        print(list_of_answers)
+    if len(list_of_answers) == 50:
+        game_is_on = False
+
+turtle.mainloop()
